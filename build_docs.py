f1a71f1d84c17a0766fb51d7749fc0a7cf1fdd60
David Hart <dbhart@sandia.gov>
2024-10-07 13:46:46 -0600

    docs: build docs with preferred latest version

--- build_docs.py
+++ build_docs.py
@@ -20,14 +20,29 @@ my_env = os.environ.copy()
 tags = git_tag.stdout.splitlines()
 versions = [
     {
-        "name": "latest",
-        "version": "latest",
-        "url": "https://sandialabs.github.io/sansmic/",
-        "preferred": True,
+        "name": "main",
+        "version": "main",
+        "url": "https://sandialabs.github.io/sansmic/main/",
+        "preferred": False,
     }
 ]
 
+found_stable = False
+latest = False
 for tag in tags:
+    if not found_stable and not "-" in tag:
+        latest = True
+        found_stable = tag
+        versions.append(
+            dict(
+                name=latest,
+                version=tag,
+                url="https://sandialabs.github.io/sansmic/",
+                preferred=True,
+            )
+        )
+    else:
+        latest = False
     versions.append(
         dict(
             name=tag,
@@ -36,13 +51,17 @@ for tag in tags:
             preferred=False,
         )
     )
+if not found_stable:
+    found_stable = "main"
 
 with open(
     os.path.abspath(os.path.join(".", "docs", "_static", "switcher.json")), "w"
 ) as fswitch:
     json.dump(versions, fswitch)
 
-my_env["SANSMIC_SPHINX_VERSION"] = "latest"
+my_env["SANSMIC_SPHINX_VERSION"] = "main"
+
+os.remove(os.path.abspath(os.path.join(".", "docs", "_static", "switcher.json")))
 
 subprocess.run(
     " ".join(
@@ -51,30 +70,20 @@ subprocess.run(
             "-b",
             "html",
             "docs/",
-            "docs/_build/html",
+            "docs/_build/html/main",
         ]
     ),
     shell=True,
     env=my_env,
 )
 
-os.remove(os.path.abspath(os.path.join(".", "docs", "_static", "switcher.json")))
-
 for tag in tags:
     os.environ["VERSION_INFO"] = repr(tag)
     my_env["SANSMIC_SPHINX_VERSION"] = tag
-    subprocess.run("git checkout " + tag, shell=True)
-    subprocess.run(
-        "git checkout main -- docs/conf.py",
-        shell=True,
-    )
-    subprocess.run(
-        "pip install --no-deps --force-reinstall --no-input --no-cache-dir -e .",
-        shell=True,
-    )
     files = glob.glob("./docs/apidocs/*.rst")
     for f in files:
         os.remove(f)
+    subprocess.run("git checkout " + tag, shell=True)
     subprocess.run(
         " ".join(
             [
@@ -88,3 +97,29 @@ for tag in tags:
         shell=True,
         env=my_env,
     )
+
+with open(
+    os.path.abspath(os.path.join(".", "docs", "_static", "switcher.json")), "w"
+) as fswitch:
+    json.dump(versions, fswitch)
+
+tag = found_stable
+os.environ["VERSION_INFO"] = repr(tag)
+my_env["SANSMIC_SPHINX_VERSION"] = tag
+files = glob.glob("./docs/apidocs/*.rst")
+for f in files:
+    os.remove(f)
+subprocess.run("git checkout " + tag, shell=True)
+subprocess.run(
+    " ".join(
+        [
+            "sphinx-build",
+            "-b",
+            "html",
+            "docs/",
+            "docs/_build/html/",
+        ]
+    ),
+    shell=True,
+    env=my_env,
+)
